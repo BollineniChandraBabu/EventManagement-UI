82d1f700e28b5f39962ee8252472309209ebb603
CHANDRA BABU BOLLINENI <45785707+BollineniChandraBabu@users.noreply.github.com>
2026-02-18 15:29:49 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://eventmanagement-backend-ka9x.onrender.com/api',
-  firebaseConfig: {
-    apiKey: 'AIzaSyBsmXsvKWJZ6thBHTHtLzaRVZ9P21MU-OI',
-    authDomain: 'familywish-817df.firebaseapp.com',
-    projectId: 'familywish-817df',
-    storageBucket: 'familywish-817df.firebasestorage.app',
-    messagingSenderId: '281687365580',
-    appId: '1:281687365580:web:5cbbd742c813423a9bef69'
-  }
+  apiUrl: 'https://eventmanagement-backend-ka9x.onrender.com/api'
 };
